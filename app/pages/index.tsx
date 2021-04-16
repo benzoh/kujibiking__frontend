54550b9b3e4c18198198e5d70534af09f30ab7ed
Benzoh <benzoh.wada@gmail.com>
2021-04-16 13:55:43 +0900

    add index page

---
+++ app/pages/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { Button } from 'react-bootstrap'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>KujibiKing</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}> "KujibiKing" </h1>
+
+        <div>
+          <img src="https://placehold.jp/250x250.png?text=DUMMY" alt=""/>
+        </div>
+
+        <div className="d-flex flex-column">
+          <div>
+            <Button href="#">ログイン</Button>
+          </div>
+          <div>
+            <Button variant="outline-primary" href="#">サインイン</Button>
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.hippohack.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © hippohack
+        </a>
+      </footer>
+    </div>
+  )
+}
