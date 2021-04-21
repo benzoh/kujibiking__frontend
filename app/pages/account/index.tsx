74236c3a8d24b546e84cfa3d51e3ade1f6818994
Benzoh <benzoh.wada@gmail.com>
2021-04-21 11:05:58 +0900

    add account mypage timeline

---
+++ app/pages/account/index.tsx
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+// import styles from '../styles/Account.module.css'
+import { Button } from 'react-bootstrap'
+
+export default function Account() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>アカウント管理 | KujibiKing</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={' '}>
+        <form>
+          <div className="form-group">
+            <label>ユーザ名</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>メールアドレス</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>SNSアカウント</label>
+            <div className="d-flex align-items-center">
+              <img src="https://placehold.jp/50x50.png?text=ICON" alt=""/>
+              <span>USERNAME</span>
+              <a className="ml-auto" href="#">解除</a>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>パスワード</label>
+            <input type="text" className="form-control" />
+          </div>
+          
+          <button type="submit" className="btn btn-primary">更新</button>
+        </form>
+
+        <div className="row mt-5">
+          <div className="col">
+            <div className="d-flex">
+              <div>退会する</div>
+              <a className="ml-auto" href="#">退会する</a>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className='mt-3'>
+        <a
+          href="https://www.hippohack.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © hippohack
+        </a>
+      </footer>
+    </div>
+  )
+}
---
+++ app/pages/mypage/index.tsx
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+// import styles from '../styles/MyPage.module.css'
+import { Button } from 'react-bootstrap'
+
+export default function MyPage() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>マイページ | KujibiKing</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={' '}>
+        <div className="row">
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <span>USERNAME</span>
+              <img src="https://placehold.jp/50x50.png?text=ICON" alt=""/>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h3>最近の登録</h3>
+            <ol>
+              <li>hoge</li>
+              <li>hoge</li>
+              <li>hoge</li>
+            </ol>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h3>ランキング</h3>
+            <div className="row">
+              <div className="col">
+                <div>●●●●●</div>
+                <div className="text-right">１位</div>
+              </div>
+              <div className="col">
+                <div>●●●●●</div>
+                <div className="text-right">１位</div>
+              </div>
+              <div className="col">
+                <div>●●●●●</div>
+                <div className="text-right">１位</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h3>グラフ？</h3>
+            <div>
+              <img className="w-100" src="https://placehold.jp/300x100.png?text=グラフ" alt=""/>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className='mt-3'>
+        <a
+          href="https://www.hippohack.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © hippohack
+        </a>
+      </footer>
+    </div>
+  )
+}
