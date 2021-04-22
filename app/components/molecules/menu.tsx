54df3acf2e587be8bbded038ea865f39f889fd98
Benzoh <benzoh.wada@gmail.com>
2021-04-22 23:14:44 +0900

    WIP: menu style

--- app/components/molecules/menu.tsx
+++ app/components/molecules/menu.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useState } from "react";
 
+import styles from "../../styles/components/molecules/Menu.module.scss";
+
 export default function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
   const menuFunction = () => {
@@ -9,45 +11,6 @@ export default function Menu() {
 
   return (
     <>
-      <style jsx>{`
-        .container {
-          margin: 0 0 0 auto;
-        }
-        .humburger {
-          display: block;
-        }
-        .humburger {
-          background-color: rgb(34, 35, 36);
-          color: white;
-          padding: 10px 5px 0 5px;
-          display: none;
-          cursor: pointer;
-          position: fixed;
-          top: 20px;
-          right: 40px;
-        }
-        .humburger span {
-          background-color: white;
-          width: 30px;
-          height: 1px;
-          display: block;
-          margin-bottom: 7px;
-        }
-        .humburger span:nth-child(1) {
-          width: 20px;
-          margin-left: 10px;
-        }
-        .humburger p {
-          font-size: 13px;
-          margin-top: -4px;
-          margin-bottom: 5px;
-        }
-        .open {
-          height: 100vh;
-          opacity: 1;
-          z-index: 100;
-        }
-      `}</style>
       <nav>
         <ul>
           <li>
@@ -83,7 +46,7 @@ export default function Menu() {
         </ul>
       </nav>
       <div className="container">
-        <div className="humburger" onClick={() => menuFunction()}>
+        <div className={styles.humburger} onClick={() => menuFunction()}>
           <span className={openMenu ? "open" : undefined}></span>
           <span className={openMenu ? "open" : undefined}></span>
           <p className={openMenu ? "open" : undefined}>Menu</p>
