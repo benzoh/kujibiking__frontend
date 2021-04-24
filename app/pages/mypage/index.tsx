cab239b09f9ef09af6d9de3222516877756dc82f
Benzoh <benzoh.wada@gmail.com>
2021-04-24 21:24:38 +0900

    add layout component

--- app/pages/mypage/index.tsx
+++ app/pages/mypage/index.tsx
@@ -1,75 +1,82 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import styles from '../styles/MyPage.module.css'
-import { Button } from 'react-bootstrap'
+import { Button } from "react-bootstrap";
+import Layout from "../../components/templates/layout";
 
 export default function MyPage() {
   return (
-    <div className='container'>
-      <Head>
-        <title>マイページ | KujibiKing</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className="container">
+        <Head>
+          <title>マイページ | KujibiKing</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={' '}>
-        <div className="row">
-          <div className="col">
-            <div className="d-flex align-items-center">
-              <span>USERNAME</span>
-              <img src="https://placehold.jp/50x50.png?text=ICON" alt=""/>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <h3>最近の登録</h3>
-            <ol>
-              <li>hoge</li>
-              <li>hoge</li>
-              <li>hoge</li>
-            </ol>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <h3>ランキング</h3>
-            <div className="row">
-              <div className="col">
-                <div>●●●●●</div>
-                <div className="text-right">１位</div>
-              </div>
-              <div className="col">
-                <div>●●●●●</div>
-                <div className="text-right">１位</div>
-              </div>
-              <div className="col">
-                <div>●●●●●</div>
-                <div className="text-right">１位</div>
+        <main className={" "}>
+          <div className="row">
+            <div className="col">
+              <div className="d-flex align-items-center">
+                <span>USERNAME</span>
+                <img src="https://placehold.jp/50x50.png?text=ICON" alt="" />
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="row">
-          <div className="col">
-            <h3>グラフ？</h3>
-            <div>
-              <img className="w-100" src="https://placehold.jp/300x100.png?text=グラフ" alt=""/>
+          <div className="row">
+            <div className="col">
+              <h3>最近の登録</h3>
+              <ol>
+                <li>hoge</li>
+                <li>hoge</li>
+                <li>hoge</li>
+              </ol>
             </div>
           </div>
-        </div>
-      </main>
 
-      <footer className='mt-3'>
-        <a
-          href="https://www.hippohack.me"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          © hippohack
-        </a>
-      </footer>
-    </div>
-  )
+          <div className="row">
+            <div className="col">
+              <h3>ランキング</h3>
+              <div className="row">
+                <div className="col">
+                  <div>●●●●●</div>
+                  <div className="text-right">１位</div>
+                </div>
+                <div className="col">
+                  <div>●●●●●</div>
+                  <div className="text-right">１位</div>
+                </div>
+                <div className="col">
+                  <div>●●●●●</div>
+                  <div className="text-right">１位</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <h3>グラフ？</h3>
+              <div>
+                <img
+                  className="w-100"
+                  src="https://placehold.jp/300x100.png?text=グラフ"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <footer className="mt-3">
+          <a
+            href="https://www.hippohack.me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            © hippohack
+          </a>
+        </footer>
+      </div>
+    </Layout>
+  );
 }
--- app/pages/regist/hit/index.tsx
+++ app/pages/regist/hit/index.tsx
@@ -1,50 +1,55 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import styles from '../styles/Hit.module.css'
-import { Button } from 'react-bootstrap'
+import { Button } from "react-bootstrap";
+import Layout from "../../../components/templates/layout";
 
 export default function Hit() {
   return (
-    <div className='container'>
-      <Head>
-        <title>当たりの登録 | KujibiKing</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className="container">
+        <Head>
+          <title>当たりの登録 | KujibiKing</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={' '}>
-        <form>
-          <div className="form-group">
-            <label>1. アイテム名</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label>2. ポイント</label>
-            <select className="custom-select">
-              <option>選択してください</option>
-              <option value="1">金のエンゼル（仮）</option>
-              <option value="2">銀のエンゼル（仮）</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>3. 画像</label>
-            <input type="file" className="form-control-file" />
-          </div>
-          <div className="form-group">
-            <label>4. MEMO</label>
-            <textarea className="form-control" rows={3}></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">登録</button>
-        </form>
-      </main>
+        <main className={" "}>
+          <form>
+            <div className="form-group">
+              <label>1. アイテム名</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>2. ポイント</label>
+              <select className="custom-select">
+                <option>選択してください</option>
+                <option value="1">金のエンゼル（仮）</option>
+                <option value="2">銀のエンゼル（仮）</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>3. 画像</label>
+              <input type="file" className="form-control-file" />
+            </div>
+            <div className="form-group">
+              <label>4. MEMO</label>
+              <textarea className="form-control" rows={3}></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              登録
+            </button>
+          </form>
+        </main>
 
-      <footer className='mt-3'>
-        <a
-          href="https://www.hippohack.me"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          © hippohack
-        </a>
-      </footer>
-    </div>
-  )
+        <footer className="mt-3">
+          <a
+            href="https://www.hippohack.me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            © hippohack
+          </a>
+        </footer>
+      </div>
+    </Layout>
+  );
 }
